cfe1658730a05950f186dfa1006823ef03a1c965
ZachMuchamore <146819131+ZachMuchamore@users.noreply.github.com>
2024-01-09 15:59:30 +0000

    terrain + anims + better movement

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -6,27 +6,73 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
+
     public Transform cam;
-    public float speed = 6f;
+    public Transform groundCheck;
+
+    public float speed = 5f;
     public float turnSmoothTime = 0.1f;
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 3f;
     float turnSmoothVelocity;
+
+    public Vector3 velocity;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
+
+    private void Start()
+    {
+
+    }
     
 
     // Update is called once per frame
     void Update()
     {
+       
+        
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
+            //third person basic movement 
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection * speed * Time.deltaTime);
+        }
+        //Ground check
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0f)
+        {
+            velocity.y = -2f;
+        }
+
+        //Jumping 
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        
+        //Gravity 
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        
+        //Sprinting 
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 15f;
+        }
+        else
+        {
+            speed = 5f;
+
         }
     }
 }
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -2,17 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class player : MonoBehaviour
 {
+    public ThirdPersonMovement tpm;
+    public bool test;
+
+    //public bool isGrounded;
+    public bool isJumping;
+
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();   
+        isJumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        if (Input.GetKey("w"))
+        {
+            anim.SetBool("walk", true);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetBool("sprint", true);
+        }
+        else
+        {
+            anim.SetBool("sprint", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && tpm.isGrounded)
+        {
+            isJumping=true;
+            tpm.isGrounded=false;
+        }
+
+        DoJump();
+
+    }
+
+    void DoJump()
+    {
+        print("vel=" + tpm.velocity.y);
+
+        if (isJumping && tpm.velocity.y <= 0)
+        {
+            anim.SetBool("jump", true);
+
+            if( tpm.isGrounded == true )
+            {
+                isJumping = false;
+                anim.SetBool("jump", false);
+            }
+        }
+
     }
 }
